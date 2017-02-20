b2123bd09c4191ecf8dc2e7f9c5108d93a3d9c8b
Ben Shope <ben@benshope.com>
2017-02-19 21:16:53 -0800

    Revert "Switch to using app engine with Firebase Auth"
    This reverts commit 7888547d50517cbf8201a27bbdcca6c912ebd672.

---
+++ frontend/src/core/firebase/config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDOKiJ_WFx7hAPE7aBmGuz3Vm8uI8KJu9o',
+  authDomain: 'coffeeiq-20a9a.firebaseapp.com',
+  databaseURL: 'https://coffeeiq-20a9a.firebaseio.com',
+  storageBucket: 'coffeeiq-20a9a.appspot.com'
+};
