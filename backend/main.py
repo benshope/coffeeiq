1aae27f67ba2577b852ed7bf5ae5e0b81ffe127a
Ben Shope <ben@benshope.com>
2017-02-19 19:40:22 -0800

    Running cron job

--- backend/main.py
+++ backend/main.py
@@ -5,15 +5,15 @@ import cgi
 import urllib
 from google.appengine.api import mail
 
-class SendMail(webapp2.RequestHandler):
-    def post(self):
-        data = [self.request.get(x) for x in ['name', 'email', 'message']]
-        mail.send_mail(sender='nimajnebs@gmail.com',
+class SendMessage(webapp2.RequestHandler):
+    def get(self):
+        mail.send_mail(
+        	sender='nimajnebs@gmail.com',
             to='nimajnebs@gmail.com',
-            subject='Ben Shope Contact Form'.format(data[0]),
-            body='From: {} | {} \nMessage: {}'.format(*data))
+            subject='Ben Shope Contact Form',
+            body='THIS IS AN EMAIL BODY')
         self.response.out.write('Thanks!  Your message has been sent.')
 
 app = webapp2.WSGIApplication([
-  ('/mail', SendMail)
+  ('/mail', SendMessage)
 ])
