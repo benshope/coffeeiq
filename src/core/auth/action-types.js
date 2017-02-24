f2fada32539245eb1c1fc9ace37ef84e01a973cf
Ben Shope <ben@benshope.com>
2017-02-24 02:06:57 -0800

    Go with other project

--- src/core/auth/action-types.js
+++
@@ -1,7 +0,0 @@
-export const INIT_AUTH = 'INIT_AUTH';
-
-export const SIGN_IN_ERROR = 'SIGN_IN_ERROR';
-export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
-
-export const SIGN_OUT_SUCCESS = 'SIGN_OUT_SUCCESS';
-export const SEND_CALENDAR_INVITES = 'SEND_CALENDAR_INVITES';
