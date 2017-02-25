129d8b625c8fc53c82d49d7479440bf21aba1e3f
Ben Shope <ben@benshope.com>
2017-02-24 22:29:14 -0800

    Revert "Go with other project"
    This reverts commit f2fada32539245eb1c1fc9ace37ef84e01a973cf.

---
+++ src/core/firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAnqY2lv5g1DsCieEWPPfnnXzUfXlNOUC4',
+  authDomain: 'coffeeiq-3d16b.firebaseapp.com',
+  databaseURL: 'https://coffeeiq-3d16b.firebaseio.com',
+  storageBucket: 'coffeeiq-3d16b.appspot.com',
+  messagingSenderId: '109223590360'
+};
