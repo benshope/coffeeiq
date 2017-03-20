ba1b0b6c3afcbf84c49e1e9dc4c49df1617edc67
Ben Shope <ben@benshope.com>
2017-03-20 03:13:09 -0700

    Kick CI

--- src/core/epics.js
+++ src/core/epics.js
@@ -3,6 +3,7 @@ import { combineEpics } from 'redux-observable';
 import { authEpics } from './auth';
 import { taskEpics } from './tasks';
 
+
 const epics = combineEpics(
   ...authEpics,
   ...taskEpics
