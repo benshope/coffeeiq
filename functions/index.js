e4ef59f65d4327a2e9dc9149ac585e9b18f0835d
lexishanson <lexishanson@gmail.com>
2017-06-04 11:42:39 -0700

    tried some things with GCF that failed, but pushing for reference.

--- functions/index.js
+++ functions/index.js
@@ -3,55 +3,94 @@
 require('@google-cloud/debug-agent').start({ allowExpressions: true });
 
 const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
 const cors = require('cors')({origin: true});
+const google = require('googleapis');
+
+const events = google.calendar('v3').events;
+const OAuth2 = google.auth.OAuth2;
+const auth = new OAuth2(
+  '515941571789-msk9v5lr3q12d9miptrn39ntvom12j3r.apps.googleusercontent.com',
+  'xT6rtV60Mvp7QX7S_BMUdLFO'
+);
 
 // Configure the email transport using the default SMTP transport and a GMail account.
 // For other types of transports such as Sendgrid see https://nodemailer.com/transports/
 // TODO: Configure the `gmail.email` and `gmail.password` Google Cloud environment variables.
-const gmailEmail = encodeURIComponent(functions.config().gmail.email);
-const gmailPassword = encodeURIComponent(functions.config().gmail.password);
-const mailTransport = nodemailer.createTransport(
-    `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+// const gmailEmail = encodeURIComponent(functions.config().firebase.apiKey);
+// const gmailPassword = encodeURIComponent(functions.config().firebase);
+// const mailTransport = nodemailer.createTransport(
+//     `smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
 
 // Your company name to include in the email
 // TODO: Change this to your app or company name to customize the email sent.
-const APP_NAME = 'CoffeeIQ';
+// const APP_NAME = 'CoffeeIQ';
+
+
+// sendCalendarInvites = () => {
+//    let headers = new Headers();
+//    headers.append('Authorization', 'Bearer ' + apiKey);
+//    headers.append('Content-Type', 'application/json');
+//    return request(
+//      'https://www.googleapis.com/calendar/v3/calendars/primary/events?sendNotifications=true', { method: 'POST', body: 'a=1' })
+//      {
+//        attachments: [],
+//        location: 'The bedroom of Lexis',
+//        summary: 'Martin Luther King, Jr. Day',
+//        description: 'An important civil rights holiday',
+//        attendees: [
+//          {email: 'nimajnebs@gmail.com'},
+//          {email: 'lexishanson@gmail.com'}
+//        ],
+//        reminders: {
+//          useDefault: true
+//        },
+//        start: {
+//          dateTime: '2017-02-27T08:00:00-07:00'
+//        },
+//        end: {
+//          dateTime: '2017-02-27T08:00:00-08:00'
+//        }
+//      },
+//      { headers: headers })
+//        .map(response => response.json())
+//        .subscribe(console.log, console.error);
+//  };
+// }
+
 
 // [START sendWelcomeEmail]
 /**
  * Sends a welcome email to new user.
  */
 // [START onCreateTrigger]
-exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
-// [END onCreateTrigger]
-  // [START eventAttributes]
-  const user = event.data; // The Firebase user.
+// exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+// // [END onCreateTrigger]
+//   // [START eventAttributes]
+//   const user = event.data; // The Firebase user.
 
-  const email = user.email; // The email of the user.
-  const displayName = user.displayName; // The display name of the user.
-  // [END eventAttributes]
+//   const email = user.email; // The email of the user.
+//   const displayName = user.displayName; // The display name of the user.
+//   // [END eventAttributes]
 
-  return sendWelcomeEmail(email, displayName);
-});
-// [END sendWelcomeEmail]
+//   return sendWelcomeEmail(email, displayName);
+// });
+// // [END sendWelcomeEmail]
 
-// [START sendByeEmail]
-/**
- * Send an account deleted email confirmation to users who delete their accounts.
- */
-// [START onDeleteTrigger]
-exports.sendByeEmail = functions.auth.user().onDelete(event => {
-// [END onDeleteTrigger]
-  const user = event.data;
+// // [START sendByeEmail]
+// /**
+//  * Send an account deleted email confirmation to users who delete their accounts.
+//  */
+// // [START onDeleteTrigger]
+// exports.sendByeEmail = functions.auth.user().onDelete(event => {
+// // [END onDeleteTrigger]
+//   const user = event.data;
 
-  const email = user.email;
-  const displayName = user.displayName;
+//   const email = user.email;
+//   const displayName = user.displayName;
 
-  return sendGoodbyEmail(email, displayName);
-});
-// [END sendByeEmail]
-
+//   return sendGoodbyEmail(email, displayName);
+// });
+// // [END sendByeEmail]
 
 
 
@@ -61,7 +100,31 @@ exports.invite = functions.https.onRequest((req, res) => {
   // const email = user.email; // The email of the user.
   //const displayName = user.displayName; // The display name of the user.
   cors(req, res, () => {
-    return console.log('Calendar invite sent', res);
+    events.list({
+      auth: auth,
+      calendarId: 'primary',
+      timeMin: (new Date()).toISOString(),
+      maxResults: 10,
+      singleEvents: true,
+      orderBy: 'startTime'
+    }, function(err, response) {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+      var events = response.items;
+      if (events.length === 0) {
+        console.log('No upcoming events found.');
+      } else {
+        console.log('Upcoming 10 events:');
+        for (var i = 0; i < events.length; i++) {
+          var event = events[i];
+          var start = event.start.dateTime || event.start.date;
+          console.log('%s - %s', start, event.summary);
+        }
+      }
+    });
+    return console.log(events, functions.config(), functions.config().gmailEmail, functions.config().firebase.apiKey);
   });
 });
 
@@ -83,36 +146,4 @@ exports.invite = functions.https.onRequest((req, res) => {
 
 
 // Sends a welcome email to the given user.
-function sendWelcomeEmail(email, displayName) {
-  const mailOptions = {
-    from: '"MyCompany" <noreply@firebase.com>',
-    to: email
-  };
-
-  // The user unsubscribed to the newsletter.
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName}!, Welcome to ${APP_NAME}. This email came from Google Cloud Functions!`;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    console.log('New welcome email sent to:' + email + 'with my Cloud Function');
-  });
-}
-
-// Sends a goodbye email to the given user.
-function sendGoodbyEmail(email, displayName) {
-  const mailOptions = {
-    from: '"MyCompany" <noreply@firebase.com>',
-    to: email
-  };
-
-  // The user unsubscribed to the newsletter.
-  mailOptions.subject = 'Bye!';
-  mailOptions.text = `Hey ${displayName}!, We confirm that we have deleted your ${APP_NAME} account.`;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    console.log('Account deletion confirmation email sent to:', email);
-  });
-}
-
-
-
-
-
+// }
