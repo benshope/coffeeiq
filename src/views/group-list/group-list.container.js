28c40ad66e43c7478c228d43592208cb080b0c75
Ben Shope <ben@benshope.com>
2017-09-04 04:17:07 -0700

    Keep login/logout working

--- src/views/group-list/group-list.container.js
+++ src/views/group-list/group-list.container.js
@@ -2,11 +2,18 @@ import React, { PropTypes } from "react";
 import { connect } from "react-redux";
 import GroupForm from "views/group-form/group-form.container";
 import GroupItem from "views/group-item/group-item.container";
+import Header from "views/header";
 
-const GroupList = ({ groups }) =>
+const GroupList = ({ groups }) => (
   <div className="group-list">
-    {groups && groups.length ? groups.map((group, i) => <GroupItem key={i} group={group} />) : <GroupForm />}
-  </div>;
+    <Header />
+    {groups && groups.length ? (
+      groups.map((group, i) => <GroupItem key={i} group={group} />)
+    ) : (
+      <GroupForm />
+    )}
+  </div>
+);
 
 GroupList.propTypes = {
   groups: PropTypes.array.isRequired
