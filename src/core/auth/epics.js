d26da87498fbbac8a497f8556ff398ec107ef0d0
Ben Shope <ben@benshope.com>
2017-09-04 22:05:05 -0700

    Improve auth epic and add some icons

--- src/core/auth/epics.js
+++ src/core/auth/epics.js
@@ -10,7 +10,7 @@ import { authActions } from "./actions";
 export const signInEpic = action$ => {
   return action$
     .filter(action => action.type === authActions.SIGN_IN)
-    .map(() => {
+    .flatMap(() => {
       let provider = new firebase.auth.GoogleAuthProvider();
       provider.addScope("https://www.googleapis.com/auth/calendar.readonly");
       provider.setCustomParameters({
@@ -19,36 +19,41 @@ export const signInEpic = action$ => {
         display: "popup",
         access_type: "offline" // eslint-disable-line camelcase
       });
-      let request = firebaseAuth.signInWithPopup(provider).then(authActions.signInSuccess, authActions.signInError);
-      return Observable.fromPromise(request);
+      let request = firebaseAuth
+        .signInWithPopup(provider)
+        .then(authActions.signInSuccess, authActions.signInError);
+      return request.then(() => go("/"), console.warn);
     })
-    .flatMap(x => x)
-    .flatMap(x => {
-      return Observable.from([go("/"), x]);
-    });
+    .filter(x => x);
 };
 
 export const signOutEpic = action$ => {
   return action$
     .filter(action => action.type === authActions.SIGN_OUT)
     .map(() => {
-      return Observable.fromPromise(firebaseAuth.signOut().then(authActions.signOutSuccess, authActions.signOutError));
+      return Observable.fromPromise(
+        firebaseAuth
+          .signOut()
+          .then(authActions.signOutSuccess, authActions.signOutError)
+      );
     })
     .flatMap(x => [go("/sign-in"), x]);
 };
 
 export const signOutSuccessEpic = action$ => {
-  return action$.filter(action => action.type === authActions.SIGN_OUT_SUCCESS).filter(() => {
-    localStorage.clear();
-    let cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-      let cookie = cookies[i];
-      let eqPos = cookie.indexOf("=");
-      let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-    return false;
-  });
+  return action$
+    .filter(action => action.type === authActions.SIGN_OUT_SUCCESS)
+    .filter(() => {
+      localStorage.clear();
+      let cookies = document.cookie.split(";");
+      for (var i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        let eqPos = cookie.indexOf("=");
+        let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      }
+      return false;
+    });
 };
 
 export const authEpics = [signInEpic, signOutEpic, signOutSuccessEpic];
