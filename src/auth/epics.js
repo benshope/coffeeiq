d42cbb20ea1a01d26bf37a48506cc477a9a42e63
Benjamin Shope <bshope@salesforce.com>
2017-10-07 00:25:30 -0700

    Minor change

--- src/auth/epics.js
+++ src/auth/epics.js
@@ -7,37 +7,25 @@ import { firebaseAuth } from "src/firebase";
 import { authActions } from "./actions";
 
 export const signInEpic = action$ =>
-  action$
-    .filter(action => action.type === authActions.SIGN_IN)
-    .flatMap(({ payload }) => {
-      let provider = new firebase.auth.GoogleAuthProvider();
-      if (payload.isAdmin) {
-        provider.addScope("https://www.googleapis.com/auth/calendar.readonly");
-      }
-      provider.setCustomParameters({
-        hd: "*",
-        prompt: "consent",
-        display: "popup",
-        access_type: "offline" // eslint-disable-line camelcase
-      });
-      return firebaseAuth
-        .signInWithPopup(provider)
-        .then(
-          x => authActions.signInSuccess(x),
-          x => authActions.signInError(x)
-        );
+  action$.filter(action => action.type === authActions.SIGN_IN).flatMap(({ payload }) => {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    if (payload.isAdmin) {
+      provider.addScope("https://www.googleapis.com/auth/calendar.readonly");
+    }
+    provider.setCustomParameters({
+      hd: "*",
+      prompt: "consent",
+      display: "popup",
+      access_type: "offline" // eslint-disable-line camelcase
     });
+    return firebaseAuth
+      .signInWithPopup(provider)
+      .then(x => authActions.signInSuccess(x), x => authActions.signInFailed(x));
+  });
 
 export const signOutEpic = action$ =>
   action$
     .filter(action => action.type === authActions.SIGN_OUT)
-    .flatMap(() =>
-      firebaseAuth
-        .signOut()
-        .then(
-          x => authActions.signOutSuccess(x),
-          x => authActions.signOutError(x)
-        )
-    );
+    .flatMap(() => firebaseAuth.signOut().then(x => authActions.signOutSuccess(x), x => authActions.signOutFailed(x)));
 
 export const authEpics = [signInEpic, signOutEpic];
