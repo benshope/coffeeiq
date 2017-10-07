8153d6879cc608e05d9d6a577c341b9374be1c4d
Benjamin Shope <bshope@salesforce.com>
2017-10-07 00:36:21 -0700

    One more try

--- src/views/components/group-page/index.js
+++ src/views/components/group-page/index.js
@@ -6,22 +6,21 @@ import { orgActions } from "src/org";
 
 const GroupPage = ({ groups, users, auth, match, toggleMembership }) => {
   const group = groups && groups[match.params.groupId];
+  const userIds = group.userIds || {};
   return (
     (group &&
       users && (
         <div className="group-page">
           <h1>
             {group.name} @ {group.location}{" "}
-            <button onClick={() => toggleMembership(auth.user.uid)}>
-              {(group.userIds || {})[auth.user.uid] ? "Join" : "Leave"}
-            </button>
+            <button onClick={() => toggleMembership(auth.user.uid)}>{userIds[auth.user.uid] ? "Join" : "Leave"}</button>
           </h1>
           <h3>Members:</h3>
           <ul className="user-list">
-            {Object.keys(group.userIds).map((uid, i) => (
-              <Link key={uid} to={`/user/${uid}`}>
+            {Object.keys(userIds).map((userId, i) => (
+              <Link key={userId} to={`/user/${userId}`}>
                 <li className="user-item">
-                  {users[uid].name} - {users[uid].email}
+                  {users[userId].name} - {users[userId].email}
                 </li>
               </Link>
             ))}
