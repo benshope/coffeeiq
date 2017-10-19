c9527e604b5af6e898f2a31d4f8bf466dacf0069
Benjamin Shope <bshope@salesforce.com>
2017-10-19 14:31:33 -0700

    Add a little copy

--- src/views/components/about-page/index.js
+++ src/views/components/about-page/index.js
@@ -7,17 +7,24 @@ import Lexis from "src/assets/images/Lexis.png";
 const AboutPage = () => {
   return (
     <div>
-      <h1>We built CoffeeIQ because....</h1>
-        <p>we iz cool</p>
+      <h1>About</h1>
+      <p>
+        At CoffeeIQ we believe people should be engaged and happy at work, and we are building tools to help every
+        company make that a reality.
+      </p>
       <div>
-        <h1>Team...</h1>
-          <h3>Ben</h3>
-            <img src={Ben} alt="ben" style={{height: 200, width: 200}} />
-          <h3>Lexis</h3>
-            <img src={Lexis} alt="lexis" style={{height: 200, width: 200}} />
+        <h1>Team</h1>
+        <div>
+          CoffeeIQ’s founders came together over a shared passion for great company culture and a desire to make work
+          great for everyone — and we are building a company that embodies these ideals while also instilling them in
+          others.
+        </div>
+        <h3>Ben</h3>
+        <img src={Ben} alt="ben" style={{ height: 200, width: 200 }} />
+        <h3>Lexis</h3>
+        <img src={Lexis} alt="lexis" style={{ height: 200, width: 200 }} />
       </div>
     </div>
-
   );
 };
 
@@ -29,7 +36,4 @@ AboutPage.propTypes = {};
 
 const mapDispatchToProps = {};
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(AboutPage);
+export default connect(null, mapDispatchToProps)(AboutPage);
