c0992edeffdd59cea6cf3b3bd5c83e89c764fe92
Benjamin Shope <bshope@salesforce.com>
2017-10-19 15:50:12 -0700

    Add group names

--- src/views/components/user-page/index.js
+++ src/views/components/user-page/index.js
@@ -2,17 +2,24 @@ import PropTypes from "prop-types";
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import { authActions } from "src/auth";
 import { orgActions } from "src/org";
 
-const UserPage = ({ users, groups, match, toggleMembership }) => {
+const UserPage = ({ auth, users, groups, match, signOut, toggleMembership }) => {
   const user = users && users[match.params.userId];
   return (
     (user && (
       <div className="user-page">
-        <h1>{user.displayName}</h1>
+        <div className="user-page-header">
+          <div className="user-page-title">
+            <h1>{user.displayName}</h1>
+          </div>
+        </div>
         <div>{user.email}</div>
+        {auth.uid === user.uid && <button onClick={signOut}>Sign Out</button>}
         <img className="user-image" alt="user" src={user.photoURL} />
         <h3>Groups</h3>
+        <ul>{Object.keys(user.groupIds || {}).map(key => <li>{groups[key] && groups[key].name}</li>)}</ul>
       </div>
     )) || <div>Loading...</div>
   );
@@ -22,6 +29,7 @@ UserPage.propTypes = {
   users: PropTypes.object,
   auth: PropTypes.object.isRequired,
   groups: PropTypes.object,
+  signOut: PropTypes.func.isRequired,
   toggleMembership: PropTypes.func.isRequired
 };
 
@@ -32,6 +40,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
+  signOut: authActions.signOut,
   toggleMembership: orgActions.toggleMembership
 };
 
