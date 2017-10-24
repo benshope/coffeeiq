00e373884f33a4d8acc3c230243ef2bb650b4512
Ben Shope <ben@benshope.com>
2017-10-23 23:59:42 -0700

    Minor changes

--- src/auth/epics.js
+++ src/auth/epics.js
@@ -1,39 +1,45 @@
 import firebase from "firebase";
 import { omit } from "lodash";
-// import { push } from "react-router-redux";
+import { push } from "react-router-redux";
 import { Observable } from "rxjs";
 import { firebaseAuth } from "src/firebase";
 import { firebaseDb } from "../firebase";
 import { authActions } from "./actions";
 import { userFromResponse } from "./utils";
 
-// export const goToGroupsPage = (action$, store) =>
-//   action$
-//     .filter(action => action.type === authActions.SIGN_IN_SUCCESS)
-//     .map(() => push("/groups"));
+export const goToGroupsPage = (action$, store) =>
+  action$ &&
+  action$
+    .filter(action => action.type === authActions.SIGN_IN_SUCCESS)
+    .map(() => push("/groups"));
 
 export const signInEpic = action$ =>
-  action$.filter(action => action.type === authActions.SIGN_IN).flatMap(({ payload }) => {
-    let provider = new firebase.auth.GoogleAuthProvider();
-    if (payload) {
-      provider.addScope("https://www.googleapis.com/auth/calendar");
-    }
-    provider.setCustomParameters({
-      hd: "*",
-      prompt: "consent",
-      display: "popup",
-      access_type: "offline"
+  action$ &&
+  action$
+    .filter(action => action.type === authActions.SIGN_IN)
+    .flatMap(({ payload }) => {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      if (payload) {
+        provider.addScope("https://www.googleapis.com/auth/calendar");
+      }
+      provider.setCustomParameters({
+        hd: "*",
+        prompt: "consent",
+        display: "popup",
+        access_type: "offline"
+      });
+      return firebaseAuth
+        .signInWithPopup(provider)
+        .then(
+          response =>
+            authActions.signInSuccess(userFromResponse(response, payload)),
+          x => authActions.signInFailed(x)
+        );
     });
-    return firebaseAuth
-      .signInWithPopup(provider)
-      .then(
-        response => authActions.signInSuccess(userFromResponse(response, payload)),
-        x => authActions.signInFailed(x)
-      );
-  });
 
 // TODO: this should be in a firebase function
 export const updateUserEpic = action$ =>
+  action$ &&
   action$
     .filter(action => action && action.type === authActions.SIGN_IN_SUCCESS)
     .map(
@@ -41,15 +47,31 @@ export const updateUserEpic = action$ =>
         new Promise((resolve, reject) =>
           firebaseDb
             .ref(`orgs/${payload.orgId}/users/${payload.uid}`)
-            .update(omit(payload, "refreshToken"), error => (error ? reject(error) : resolve(payload)))
+            .update(
+              omit(payload, "refreshToken"),
+              error => (error ? reject(error) : resolve(payload))
+            )
         )
     )
     .map(payload => authActions.updateUserSuccess(payload))
     .catch(error => Observable.of(authActions.updateUserFailed(error)));
 
 export const signOutEpic = action$ =>
+  action$ &&
   action$
     .filter(action => action && action.type === authActions.SIGN_OUT)
-    .flatMap(() => firebaseAuth.signOut().then(x => authActions.signOutSuccess(x), x => authActions.signOutFailed(x)));
+    .flatMap(() =>
+      firebaseAuth
+        .signOut()
+        .then(
+          x => authActions.signOutSuccess(x),
+          x => authActions.signOutFailed(x)
+        )
+    );
 
-export const authEpics = [signOutEpic, signInEpic, updateUserEpic];
+export const authEpics = [
+  goToGroupsPageEpic,
+  signOutEpic,
+  signInEpic,
+  updateUserEpic
+];
--- src/org/epics.js
+++ src/org/epics.js
@@ -20,10 +20,18 @@ export const orgFirebaseUpdatesEpic = action$ =>
       });
       const ref = firebaseDb.ref(["orgs", orgId].filter(x => x).join("/"));
       return Observable.create(observer => {
-        ref.once("value", x => observer.next(orgActions.onValue(unwrap(x.val()))));
-        ref.on("child_added", x => observer.next(orgActions.onChildAdded(unwrap(x.val(), x.key))));
-        ref.on("child_changed", x => observer.next(orgActions.onChildChanged(unwrap(x.val(), x.key))));
-        ref.on("child_removed", x => observer.next(orgActions.onChildRemoved(unwrap(x.val(), x.key))));
+        ref.once("value", x =>
+          observer.next(orgActions.onValue(unwrap(x.val())))
+        );
+        ref.on("child_added", x =>
+          observer.next(orgActions.onChildAdded(unwrap(x.val(), x.key)))
+        );
+        ref.on("child_changed", x =>
+          observer.next(orgActions.onChildChanged(unwrap(x.val(), x.key)))
+        );
+        ref.on("child_removed", x =>
+          observer.next(orgActions.onChildRemoved(unwrap(x.val(), x.key)))
+        );
       });
     });
 
@@ -67,18 +75,22 @@ export const createInviteEpic = (action$, store) =>
     .catch(error => Observable.of(orgActions.createInviteFailed({ error })));
 
 export const createInviteErrorEpic = (action$, store) =>
-  action$.filter(action => action.type === orgActions.CREATE_INVITE_FAILED).map(({ payload }) =>
-    notificationsActions.requestCreateErrorNotification({
-      message: `Error inviting ${payload.email}: ${payload.error}`
-    })
-  );
+  action$
+    .filter(action => action.type === orgActions.CREATE_INVITE_FAILED)
+    .map(({ payload }) =>
+      notificationsActions.requestCreateErrorNotification({
+        message: `Error inviting ${payload.email}: ${payload.error}`
+      })
+    );
 
 export const createInviteSuccessEpic = (action$, store) =>
-  action$.filter(action => action.type === orgActions.CREATE_INVITE_SUCCESS).map(() =>
-    notificationsActions.requestCreateSuccessNotification({
-      message: "Invite sent"
-    })
-  );
+  action$
+    .filter(action => action.type === orgActions.CREATE_INVITE_SUCCESS)
+    .map(() =>
+      notificationsActions.requestCreateSuccessNotification({
+        message: "Invite sent"
+      })
+    );
 
 export const createGroupEpic = (action$, store) =>
   action$
@@ -97,14 +109,16 @@ export const createGroupEpic = (action$, store) =>
     .catch(error => Observable.of(orgActions.createGroupFailed(error)));
 
 export const createGroupSuccessEpic = (action$, store) =>
-  action$.filter(action => action.type === orgActionTypes.CREATE_GROUP_SUCCESS).flatMap(() =>
-    Observable.from([
-      notificationsActions.requestCreateSuccessNotification({
-        message: "Group created"
-      })
-      // , push(`/group/${payload}`)
-    ])
-  );
+  action$
+    .filter(action => action.type === orgActionTypes.CREATE_GROUP_SUCCESS)
+    .flatMap(() =>
+      Observable.from([
+        notificationsActions.requestCreateSuccessNotification({
+          message: "Group created"
+        })
+        // , push(`/group/${payload}`)
+      ])
+    );
 
 // export const updateGroupEpic = (action$, store) =>
 //   action$.filter(action => action.type === orgActionTypes.UPDATE_GROUP).flatMap(({ payload }) => {
@@ -124,33 +138,46 @@ export const deleteGroupEpic = (action$, store) =>
       const groupId = payload;
       const state = store.getState();
       let updates = {};
-      updates[`groups`] = omit((state.org[state.auth.orgId] || {}).groups, groupId);
-      Object.keys((state.org[state.auth.orgId] || {}).groups[groupId].userIds || {}).forEach(userId => {
+      updates[`groups`] = omit(
+        (state.org[state.auth.orgId] || {}).groups,
+        groupId
+      );
+      Object.keys(
+        (state.org[state.auth.orgId] || {}).groups[groupId].userIds || {}
+      ).forEach(userId => {
         updates[`users/${userId}/groupIds`] = omit(
           (state.org[state.auth.orgId] || {}).users[userId].groupIds || {},
           groupId
         );
       });
       return new Promise((resolve, reject) =>
-        firebaseDb.ref(`orgs/${state.auth.orgId}`).update(updates, error => (error ? reject(error) : resolve(groupId)))
+        firebaseDb
+          .ref(`orgs/${state.auth.orgId}`)
+          .update(updates, error => (error ? reject(error) : resolve(groupId)))
       );
     })
     .map(groupId => orgActions.deleteGroupSuccess(groupId))
     .catch(error => Observable.of(orgActions.deleteGroupFailed(error)));
 
 export const deleteGroupSuccessEpic = (action$, store) =>
-  action$.filter(action => action.type === orgActionTypes.DELETE_GROUP_SUCCESS).flatMap(({ payload }) =>
-    Observable.from([
-      push("/groups"),
-      notificationsActions.requestCreateSuccessNotification({
-        message: "Group deleted"
-      })
-    ])
-  );
+  action$
+    .filter(action => action.type === orgActionTypes.DELETE_GROUP_SUCCESS)
+    .flatMap(({ payload }) =>
+      Observable.from([
+        push("/groups"),
+        notificationsActions.requestCreateSuccessNotification({
+          message: "Group deleted"
+        })
+      ])
+    );
 
 export const updateCalendarAccess = action$ =>
   action$
-    .filter(action => action.type === authActions.SIGN_IN_SUCCESS && action.payload.gaveCalendarAccess)
+    .filter(
+      action =>
+        action.type === authActions.SIGN_IN_SUCCESS &&
+        action.payload.gaveCalendarAccess
+    )
     .map(
       ({ payload }) =>
         new Promise((resolve, reject) =>
@@ -167,21 +194,27 @@ export const updateCalendarAccess = action$ =>
         )
     )
     .map(payload => orgActions.updateCalendarAccessSuccess(payload))
-    .catch(error => Observable.of(orgActions.updateCalendarAccessFailed(error)));
+    .catch(error =>
+      Observable.of(orgActions.updateCalendarAccessFailed(error))
+    );
 
 export const updateCalendarAccessErrorEpic = (action$, store) =>
-  action$.filter(action => action.type === orgActions.UPDATE_CALENDAR_ACCESS_ERROR).map(({ payload }) =>
-    notificationsActions.requestCreateErrorNotification({
-      message: `Error granting calendar access: ${payload}`
-    })
-  );
+  action$
+    .filter(action => action.type === orgActions.UPDATE_CALENDAR_ACCESS_ERROR)
+    .map(({ payload }) =>
+      notificationsActions.requestCreateErrorNotification({
+        message: `Error granting calendar access: ${payload}`
+      })
+    );
 
 export const updateCalendarAccessSuccessEpic = (action$, store) =>
-  action$.filter(action => action.type === orgActions.UPDATE_CALENDAR_ACCESS_SUCCESS).map(() =>
-    notificationsActions.requestCreateSuccessNotification({
-      message: "Calendar access granted"
-    })
-  );
+  action$
+    .filter(action => action.type === orgActions.UPDATE_CALENDAR_ACCESS_SUCCESS)
+    .map(() =>
+      notificationsActions.requestCreateSuccessNotification({
+        message: "Calendar access granted"
+      })
+    );
 
 export const orgEpics = [
   createGroupEpic,
