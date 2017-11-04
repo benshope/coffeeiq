862f8110b9a805b1696ef4d646c7fa73021b680b
Benjamin Shope <bshope@salesforce.com>
2017-11-04 13:30:08 -0700

    Keep button styling working

--- src/views/components/joined-group-page/index.js
+++ src/views/components/joined-group-page/index.js
@@ -5,12 +5,13 @@ import SignUpButton from "../sign-up-button";
 
 const JoinedGroupPage = ({ match }) => {
   const email = match.params.emailId.split("_").join(".");
+  const name = email.split("@")[0];
   const emailParts = match.params.emailId.split("_");
   const orgName = emailParts[emailParts.length - 2];
   return (
     <div className="joined-group-page">
       <BlueStripes />
-      <h1>Congrats {email}!</h1>
+      <h1>Congrats {name}!</h1>
       <div className="message">
         You are now a member of{match.params.groupName}.{" "}
         <b>You will receive weekly calendar invites to meet for coffee at {match.params.groupLocation}.</b> Sign up to
